bd94650eec618b70d6f0e2fffbbc74353dab13af
Srihari <srihariprasad.gv@gmail.com>
2022-03-02 18:51:33 +0530

    Update MainAbility.java

--- entry/src/main/java/com/cyb0rg/neumorphism/MainAbility.java
+++ entry/src/main/java/com/cyb0rg/neumorphism/MainAbility.java
@@ -12,6 +12,8 @@
 //        See the License for the specific language governing permissions and
 //        limitations under the License.package com.cyb0rg.neumorphism;
 
+package com.cyb0rg.neumorphism;
+
 import ohos.ace.ability.AceAbility;
 import ohos.aafwk.content.Intent;
 
